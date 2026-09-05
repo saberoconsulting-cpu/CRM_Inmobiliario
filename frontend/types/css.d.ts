790049d2d6305f1b820cc73c6a9add2d5e9b7885
923327532 <roberto.lopez@tecsup.edu.pe>
2026-09-05 02:00:03 -0500

    subiendo

---
+++ frontend/types/css.d.ts
@@ -0,0 +1,5 @@
+// types.d.ts
+// Acepta imports de estilos (Next/Tailwind y CSS de librerías que cargan su hoja).
+declare module '*.css';
+declare module '*.css?inline';
+declare module 'maplibre-gl/dist/maplibre-gl.css';
